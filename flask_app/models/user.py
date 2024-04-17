5e7a3b61431cdbdf0e46986beb7c8de2cbb1802b
RBoyens <142549228+RBoyens@users.noreply.github.com>
2024-04-16 21:56:32 -0400

    API practice for Golf web

---
+++ flask_app/models/user.py
@@ -0,0 +1,5 @@
+from flask_app.config import connectToMySQL
+
+class User:
+    def __init__(self,data) -> None:
+        pass
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask_app import app
+# be sure to import flask_app.controller so the routes will work
 
 if __name__ == '__main__':
     app.run(debug=True)
