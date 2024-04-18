36e65b62ab53d0e456dd4c8dddea85d7ebe13dda
RBoyens <142549228+RBoyens@users.noreply.github.com>
2024-04-18 12:48:34 -0400

    API practice with Golf web app

---
+++ flask_app/models/golf_api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_app.config import connectToMySQL
+import requests
+import json
+
+#API to get all courses
+def get_all_courses ():
+    pass
+
+#API to get course details
+def get_course_detail ():
+    pass
+
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,5 @@
-from flask_app.config import connectToMySQL
+from flask import Flask
+from flask_app.config.mysqlconnection import connectToMySQL
 
 class User:
     def __init__(self,data):
@@ -8,19 +9,25 @@ class User:
         self.email = data['email']
         self.password = data ['password']
 
+    #Save(Create New User)
+    @classmethod
+    def save_new_user(cls,data):
+        query='''INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s)'''
+        return connectToMySQL('golf_schema').query_db(query,data)
+
     #Get All Users
     @classmethod
     def get_all_users(cls):
-        pass
+        query = '''SELECT * FROM users'''
+        result = connectToMySQL('golf_schema').query_db(query)
+        users = []
+        for user in result:
+            users.append(cls(user))
+        return users
     
     #Get User and Their Favorites
     @classmethod
     def users_with_favorites(cls):
-        pass
-    
-    #Save(Create New User)
-    @classmethod
-    def save_new_user(cls,data):
         pass
     
     #Update
