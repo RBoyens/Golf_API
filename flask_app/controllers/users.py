b13a107df7732a0254fa9fb4a920930a948de307
RBoyens <142549228+RBoyens@users.noreply.github.com>
2024-05-23 10:37:49 -0400

    Working with Registration and Session

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template,request, redirect, flash, session
 from flask_app import app
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
+bcrypt=Bcrypt(app)
 
 @app.route("/")
+def index ():
+    return redirect('/home')
+
+@app.route('/home')
 def Login ():
     return render_template('login.html')
 
-@app.route("/register", methods=['POST']) 
+@app.route('/register', methods=['POST']) 
 def register_user():
     if not User.validate_user(request.form):
         return redirect('/')
@@ -22,7 +27,7 @@ def register_user():
     if user_in_db:
         flash('Email already exists')
         return redirect ('/')
-    pw_hash = Bcrypt.generate_password_hash(request.form['password'])
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
     data = {
         "first_name":request.form['first_name'],
         "last_name":request.form['last_name'],
@@ -42,8 +47,13 @@ def login_user():
     if not user_in_db:
         flash("Invalid Email or Password")
         return redirect('/')
-    if not Bcrypt.check_password_hash(user_in_db.password,request.form['password']):
+    if not bcrypt.check_password_hash(user_in_db.password,request.form['password']):
         flash("Invalid Email or Password")
         return redirect('/')
     session['id'] = user_in_db
     return redirect ('/dashboard')
+
+@app.route('/logout')
+def logout():
+    session.clear
+    redirect('/')
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask_app import app
 # be sure to import flask_app.controller so the routes will work
 from flask_app.controllers import users
+from flask_app.controllers import golf_apis
 
 if __name__ == '__main__':
     app.run(debug=True)
